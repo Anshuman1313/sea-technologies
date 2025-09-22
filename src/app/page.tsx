81ba4a2a5cc9337fc984c9347ff2d487f00faaf7
Anshuman1313 <anshumanrana0007@gmail.com>
2025-09-22 12:40:09 +0530

    Theme-contact-us-Newsletter
    I have made theme toggle contact-us and Newsletter.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import HeroSection from "@/components/landingpage/HeroSection";
 import { Industries } from "@/components/landingpage/Industries";
 import LogoSection from "@/components/landingpage/LogoSection";
 import { MovingLogos } from "@/components/landingpage/MovingLogos";
+import { Newsletter } from "@/components/landingpage/NewsLetter";
 import Navbar from "@/components/layouts/Navbar";
 import { CpuLines2 } from "@/components/practicemotion/SvgLines/CpuLines2";
 import { div } from "motion/react-client";
@@ -22,6 +23,9 @@ export default function Home() {
            
         <div className="bg-white ">
           <Industries />
+        </div>
+        <div className="bg-white my-20 ">
+          <Newsletter/>
         </div>
     </div>
   );
