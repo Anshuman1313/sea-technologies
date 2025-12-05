9d152844420f0b6e8776d4e7dc8cc90da49e3b3e
Anshuman1313 <168716686+Anshuman1313@users.noreply.github.com>
2025-12-05 19:04:40 +0530

    SmallChanges
    Banner email corrected, Newsletter padding fixed footer privacy text fixed

--- src/components/landingpage/Banner.tsx
+++ src/components/landingpage/Banner.tsx
@@ -9,7 +9,7 @@ const Banner = () => {
         <div className="bg-gradient-to-b from-blue-500 to-blue-600 px-2 py-2 font-sans text-xs font-medium tracking-tight text-white sm:px-4 sm:py-3 sm:text-sm md:text-right flex flex-col md:flex-row md:justify-end md:items-center gap-2">
              <span className='flex items-center md:justify-around'> 
               <Mail className='size-[18px] mx-1 '/> 
-              <Link href="mailto:sales@seasmartz.com" className="underline hover:text-gray-200">sales@seasmartz.com</Link></span>
+              <Link href="mailto:sales@seatechnologies.in" className="underline hover:text-gray-200">sales@seatechnologies.in</Link></span>
             <span className="hidden md:inline-block">|</span>
             <span className='flex items-center md:justify-around'> <CircleUser className='size-[18px] mx-1  '/>  <Link href="tel:9056641707" className="underline hover:text-gray-200">+91 90566-41707</Link></span>
            
